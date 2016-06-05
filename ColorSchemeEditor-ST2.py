75a7399c33f46d9418a6bf2f40aaf93c511fc119
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2016-06-05 13:21:44 -0400

    Quick cleanups
     * Remove trailing whitespace
     * Normalize newlines
     * Modify comment placement/contents
     * Remove commented-out print statements

--- ColorSchemeEditor-ST2.py
+++ ColorSchemeEditor-ST2.py
@@ -7,13 +7,14 @@ _wasSingleLayout = None
 _lastScope = None
 _lastScopeIndex = 0
 
+
 def find_matches ( scope, founds ):
 	global _schemeEditor
 
 	ret = []
 	maxscore = 0
-	# find the scope in the xml that matches the most
 
+	# find the scope in the xml that matches the most
 	for found in founds:
 		foundstr = _schemeEditor.substr( found )
 		pos = foundstr.find( '<string>' ) + 8
@@ -26,7 +27,6 @@ def find_matches ( scope, founds ):
 			padleft = fstrlen - len( fstr )
 			fstr = fstr.rstrip( ' ' )
 			score = sublime.score_selector( scope, fstr )
-			# print( fstr, score )
 			if score > 0:
 				a = found.a + pos + padleft
 				ret.append( [ score, sublime.Region( a, a + len( fstr ) ) ] )
@@ -37,9 +37,11 @@ def find_matches ( scope, founds ):
 	else:
 		return ret
 
+
 def display_scope ( region ):
 	global _schemeEditor
-	# doest change the selection if previous selection was on the same line
+
+	# doesn't change the selection if previous selection was on the same line
 	sel = _schemeEditor.sel()
 	sel.clear()
 	sel.add( region )
@@ -47,18 +49,17 @@ def display_scope ( region ):
 
 
 def update_view_status ( view ):
-
 	global _lastScope, _lastScopeIndex
 
 	found = None
 	_lastScope = []
 	_lastScopeIndex = 0
-	
+
 	# find the scope under the cursor
 	scope_name = view.scope_name( view.sel()[0].a )
 	pretty_scope = scope_name.strip( ' ' ).replace( ' ', ' > ' )
 	scopes = reversed( pretty_scope.split( ' > ' ) )
-	
+
 	# convert to regex and look for the scope in the scheme editor
 	for scope in scopes:
 		if len( scope ) == 0:
@@ -71,14 +72,11 @@ def update_view_status ( view ):
 			dots -= 1
 		regex += ')( ?, ?[a-z\\.\\-]*)*</string>'
 
-		# print( regex )
 		found = _schemeEditor.find_all( regex, 0 )
 		found = find_matches( scope_name, found )
-		# print( found )
 		if found != None:
 			_lastScope += found
 
-	# print( _lastScope )
 	scopes = len( _lastScope )
 	sublime.status_message( 'matches ' + str( scopes ) + ': ' + pretty_scope )
 	if scopes == 0:
@@ -105,17 +103,20 @@ def kill_scheme_editor ():
 	_lastScopeIndex = 0
 
 
+
 # listeners to update our scheme editor
 class NavigationListener ( sublime_plugin.EventListener ):
 
 	def on_close ( self, view ):
 		global _schemeEditor
+
 		if _schemeEditor != None:
 			if _schemeEditor.id() == view.id():
 				kill_scheme_editor()
 
 	def on_selection_modified ( self, view ):
 		global _schemeEditor, _skipOne
+
 		if _schemeEditor != None:
 			if _schemeEditor.id() != view.id() and not view.settings().get( 'is_widget' ):
 				# for some reason this callback is called twice - for mouse down and mouse up
@@ -126,7 +127,9 @@ class NavigationListener ( sublime_plugin.EventListener ):
 					update_view_status( view )
 
 
+
 class EditColorSchemeNextScopeCommand ( sublime_plugin.TextCommand ):
+
 	def run ( self, edit ):
 		global _schemeEditor, _lastScope, _lastScopeIndex
 
@@ -142,6 +145,7 @@ class EditColorSchemeNextScopeCommand ( sublime_plugin.TextCommand ):
 
 
 class EditColorSchemePrevScopeCommand ( sublime_plugin.TextCommand ):
+
 	def run ( self, edit ):
 		global _schemeEditor, _lastScope, _lastScopeIndex
 
@@ -157,10 +161,10 @@ class EditColorSchemePrevScopeCommand ( sublime_plugin.TextCommand ):
 
 
 class EditCurrentColorSchemeCommand ( sublime_plugin.TextCommand ):
-	
+
 	def run ( self, edit ):
 		global _schemeEditor, _wasSingleLayout
-		
+
 		view = self.view
 		viewid = view.id()
 		window = view.window()
@@ -210,7 +214,7 @@ class EditCurrentColorSchemeCommand ( sublime_plugin.TextCommand ):
 			else:
 				#if the editor is in different splitter already focus it
 				window.focus_view( _schemeEditor )
-			
+
 			window.focus_view( view )
 			update_view_status( view )
 
@@ -223,7 +227,3 @@ class EditCurrentColorSchemeCommand ( sublime_plugin.TextCommand ):
 					'cells': [[0, 0, 1, 1]]
 				} )
 			kill_scheme_editor()
-			
-
-		
-		
